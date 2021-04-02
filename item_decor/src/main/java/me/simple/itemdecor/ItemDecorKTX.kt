b599f98b793f7bda37d92beef8d580df8752f302
chenpeng <383559698@qq.com>
2021-04-02 11:47:50 +0800

    更新：KTX

--- item_decor/src/main/java/me/simple/itemdecor/ItemDecorKTX.kt
+++ item_decor/src/main/java/me/simple/itemdecor/ItemDecorKTX.kt
@@ -7,13 +7,14 @@ fun RecyclerView.divider(
     color: Int = Color.LTGRAY,
     size: Int = 1,
     margin: Float = 0f
-) {
+): LinearItemDecor {
     val itemDecor = LinearItemDecor().apply {
         this.color = color
         this.size = size
         this.margin = margin
     }
     this.addItemDecoration(itemDecor)
+    return itemDecor
 }
 
 fun RecyclerView.divider(
@@ -21,7 +22,7 @@ fun RecyclerView.divider(
     size: Int = 1,
     marginStart: Float = 0f,
     marginEnd: Float = 0f
-) {
+): LinearItemDecor {
     val itemDecor = LinearItemDecor().apply {
         this.color = color
         this.size = size
@@ -29,10 +30,15 @@ fun RecyclerView.divider(
         this.marginEnd = marginEnd
     }
     this.addItemDecoration(itemDecor)
+    return itemDecor
 }
 
 fun RecyclerView.space(
-    space: Int
-) {
-
+    margin: Int
+): GridItemDecor {
+    val itemDecor = GridItemDecor().apply {
+        this.margin = margin
+    }
+    this.addItemDecoration(itemDecor)
+    return itemDecor
 }
